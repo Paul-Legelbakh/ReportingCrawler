9a8a83268a4f80a2b95c416c3c34853845757593
EtherDrake <you@example.com>
2018-01-22 14:24:14 +0200

    TestView

---
+++ ReportingDataBase/Context/MongoDBContext.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using ReportingDataBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportingDataBase.Context
+{
+    public class MongoDBContext
+    {
+        MongoClient mongoClient = new MongoClient("mongodb://192.168.128.231:27017");
+
+        public MongoDBContext()
+        {
+
+        }
+
+
+        public List<Crawler> GetCrawlers()
+        {
+            var db = mongoClient.GetDatabase("crawler");
+            var crawlers = db.GetCollection<Crawler>("crawler");
+            return crawlers.Find(new BsonDocument()).ToList();
+        }
+
+        public List<Vacancy> GetVacancies()
+        {
+            var db = mongoClient.GetDatabase("crawler");
+            var vacancies = db.GetCollection<Vacancy>("vacancy");
+            return vacancies.Find(new BsonDocument()).ToList();
+        }
+    }
+}
